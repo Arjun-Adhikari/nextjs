3eb8f5d01938ef021b0930c710417d65ee868303
Arjun Adhikari <arjun610705@gmail.com>
2026-08-17 16:37:50 +0545

    refactor:db table name

--- src/lib/actions/todos.ts
+++ src/lib/actions/todos.ts
@@ -3,11 +3,11 @@ import { query } from "../db";
 
 export async function createTodo(prevState: any, formData: FormData) {
   const savedData = await query(
-    "INSERT INTO todos (firstname, lastname, photo) VALUES ($1, $2, $3) RETURNING *",
+    "INSERT INTO users (firstname, lastname, photo) VALUES ($1, $2, $3) RETURNING *",
     [
       formData.get("firstname"),
       formData.get("lastname"),
-      formData.get("photo"),
+      formData.get("photokey"),
     ],
   );
   return savedData.rows;
